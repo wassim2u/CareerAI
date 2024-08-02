27944ca18f9c89f1eee612bdd664ed55e9c76f0a
Wassim Jabrane <wassim.jabrane@gmail.com>
2024-08-02 01:06:41 +0100

    Included and modifed landing page design with text + landing page image

--- UI/flutter_app/lib/main.dart
+++ UI/flutter_app/lib/main.dart
@@ -1,9 +1,12 @@
 @JS()
 library gradient.js;
+
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_app/firebase_options.dart';
 import 'package:js/js.dart';
-
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -13,49 +16,34 @@ import 'package:timelines/timelines.dart';
 
 import 'widget.dart';
 
-
-
-
-
-
 Future<void> main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
-  try{
+  try {
     await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+      options: DefaultFirebaseOptions.currentPlatform,
     );
   } on FirebaseException catch (e) {
     if (e.code == 'duplicate-app') {
       await Firebase.initializeApp();
     }
   }
-  
+
   runApp(MyApp());
   SemanticsBinding.instance.ensureSemantics();
-
 }
-
-
-
-
-
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'Ace',
       theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlueAccent),
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlueAccent),
       ),
-      darkTheme: ThemeData.dark(),
+      // darkTheme: ThemeData.dark(),
       onGenerateRoute: (settings) {
         String? path = Uri.tryParse(settings.name!)?.path;
         Widget child;
@@ -77,30 +65,109 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
-class WelcomePage extends StatelessWidget{
+class WelcomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(children: [
-        Text("Ace your next interview."),
-        ElevatedButton(onPressed: () => _navigateToNextScreen(context), 
-        child: Text("Get Started"))
-      ],)
-    );  
+      body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+          // mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+                padding: EdgeInsets.fromLTRB(30, 0, 10, 0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  // crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                   FittedBox(fit:BoxFit.scaleDown, child:Wrap(
+                       
+                      direction: Axis.vertical, children: [
+                     
+                      Text.rich(TextSpan(children: <InlineSpan>[
+                        TextSpan(
+                          text: 'In',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(
+                          text: "Vision\n",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        )
+                      ])),
+                      AutoSizeText(
+                        'Ace your next ',
+                        minFontSize: 50,
+                        maxFontSize: 120,
+                        style: TextStyle(height:0.04)
+                      ),
+                      AutoSizeText('interviews.',
+                          minFontSize: 50,
+                          maxFontSize: 120,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 150,
+                              color: Colors.blue)),
+                    ])),
+                    SizedBox(height:10),
+                    FittedBox(
+                        // padding:EdgeInsets.all(1),
+                        // alignment: Alignment.bottomCenter,
+                        child: Wrap(
+                          // mainAxisAlignment: MainAxisAlignment.end,
+                          // crossAxisAlignment: ,
+                          direction: Axis.vertical,
+                          spacing: 10,
+                          
+
+                          children: [
+                            AutoSizeText(
+                                "Real-time, Personalized Feedback \n Receive instant, AI-generated insights on your interview performance, \n helping you identify strengths and areas for improvement with precision."),
+                            AutoSizeText(
+                                "Customizable Feedback Options \n Tailor the feedback you receive by selecting specific areas you want to focus on, \n whether it is quick resume analysis, behavourial interviews, or technical interviews."),
+                          ],
+                        ),),
+
+                    SizedBox(height:10),
+
+                    Row(mainAxisAlignment: MainAxisAlignment.start,children:[ElevatedButton(
+
+                        onPressed: () => _navigateToNextScreen(context),
+                        child: RichText(
+  text: TextSpan(
+    style: Theme.of(context).textTheme.bodyLarge,
+    children: [
+      TextSpan(text: 'Get Started '),
+      WidgetSpan(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(),
+          child: Icon(Icons.arrow_right_alt_rounded),
+        ),
+      ),
+    ],
+  ),
+))])
+                  ],
+                )),
+
+            //   Container(
+            //   child: Image(image: AssetImage('assets/landing_page.jpeg')),
+            // ),
+            Image(
+                image: AssetImage('assets/landing_page.jpeg'),
+                fit: BoxFit.scaleDown,
+                )
+          ]
+          ),
+    );
   }
 
   void _navigateToNextScreen(BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) => ProcessTimelinePage()));
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => ProcessTimelinePage()));
   }
 }
-
-
-
-
-
-
 
 class HomePage extends StatefulWidget {
   HomePage({
@@ -125,16 +192,14 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-
-  
-
   @override
   Widget build(BuildContext context) {
     return PopScope(
       canPop: false,
-        onPopInvoked: (bool didPop) async {if (_navigatorKey.currentState?.canPop() ?? false) {
+      onPopInvoked: (bool didPop) async {
+        if (_navigatorKey.currentState?.canPop() ?? false) {
           _navigatorKey.currentState?.maybePop();
-        } 
+        }
       },
       child: Column(
         children: [
