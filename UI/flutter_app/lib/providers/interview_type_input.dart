1abe77a82368cefa4301a568584e0fd73e62ecd8
Wassim Jabrane <wassim.jabrane@gmail.com>
2024-08-05 18:19:21 +0100

    Added different feedback options + included next and back button for all pages for the process timeline

--- UI/flutter_app/lib/providers/interview_type_input.dart
+++ UI/flutter_app/lib/providers/interview_type_input.dart
@@ -5,7 +5,6 @@ import 'package:flutter_app/firebase_options.dart';
 import 'package:flutter_app/models/job_feedback.dart';
 import 'package:http/http.dart' as http;
 
-
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -13,18 +12,19 @@ import 'package:file_picker/file_picker.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:mime/mime.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+final interviewFeedbackTypes = ["Quick", "Behavourial", "Technical"];
 
-
-
-
+final List<Widget> _interviewFeedbackTypeTextWidgets = <Widget>[
+  Text.rich(TextSpan(style: TextStyle(color: Colors.black) ,children:[TextSpan(text: "${interviewFeedbackTypes[0]}\n", style: TextStyle(fontWeight:  FontWeight.bold)), TextSpan(text:"Rapid assessment of job-resume fit. Identifies key strengths, weaknesses, and overall suitability.")])),
+  Text.rich(TextSpan(style: TextStyle(color: Colors.black) ,children:[TextSpan(text: "${interviewFeedbackTypes[1]}\n", style: TextStyle(fontWeight:  FontWeight.bold)), TextSpan(text:"Simulated behavioral interview assessing cultural fit and behavioral competencies. Offers feedback on communication skills, problem-solving, and decision-making abilities.", )])),
+  Text.rich(TextSpan(style: TextStyle(color: Colors.grey) ,children:[TextSpan(text: "${interviewFeedbackTypes[2]} (Coming Soon)\n"), TextSpan(text:"Simulated coding challenge evaluating problem-solving, algorithm design, communication (i.e asking clarifying questions), and coding proficiency.", style: TextStyle(color: Colors.grey))])),
+];
 
 class InterviewTypeInputPage extends StatelessWidget {
-
   final int processIndex;
   final Function moveToNextOrPrevFormOnClick;
 
@@ -34,25 +34,19 @@ class InterviewTypeInputPage extends StatelessWidget {
     required this.moveToNextOrPrevFormOnClick,
   });
 
-
-
-
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-    create: (context) => InterviewTypeInputState(),
-    child:Padding(
-      padding: const EdgeInsets.all(32),
-      child: InterviewInputArea(processIndex: processIndex,
-              moveToNextOrPrevFormOnClick: moveToNextOrPrevFormOnClick)
-    )
-    );
+        create: (context) => InterviewTypeInputState(),
+        child: Padding(
+            padding: const EdgeInsets.all(32),
+            child: InterviewInputArea(
+                processIndex: processIndex,
+                moveToNextOrPrevFormOnClick: moveToNextOrPrevFormOnClick)));
   }
 }
 
-
-class InterviewInputArea extends StatefulWidget{
-
+class InterviewInputArea extends StatefulWidget {
   final int processIndex;
   final Function moveToNextOrPrevFormOnClick;
   const InterviewInputArea(
@@ -61,8 +55,6 @@ class InterviewInputArea extends StatefulWidget{
       required this.moveToNextOrPrevFormOnClick})
       : super(key: key);
 
-
-
   @override
   State<InterviewInputArea> createState() => _InterviewInputArea();
 }
@@ -70,76 +62,96 @@ class InterviewInputArea extends StatefulWidget{
 class InterviewTypeInputState extends ChangeNotifier {
   String interviewType = "";
 
-  void setInterviewType(String type){
+  void setInterviewType(String type) {
     interviewType = type;
     notifyListeners();
   }
 }
 
-
 class _InterviewInputArea extends State<InterviewInputArea> {
-    late int processIndexCopy;
-    late Function moveToNextOrPrevFormOnClickCopy;
+  late int processIndexCopy;
+  late Function moveToNextOrPrevFormOnClickCopy;
 
-    @override
-    void initState() {
-      super.initState();
-      moveToNextOrPrevFormOnClickCopy = widget.moveToNextOrPrevFormOnClick;
-      processIndexCopy = widget.processIndex;
-    }
+  @override
+  void initState() {
+    super.initState();
+    moveToNextOrPrevFormOnClickCopy = widget.moveToNextOrPrevFormOnClick;
+    processIndexCopy = widget.processIndex;
+  }
 
-  
-   
-  
+  final List<bool> selectedFeedback = <bool>[true, false, false];
+
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<InterviewTypeInputState>();
 
     IconData icon;
-  
-    
+    final ThemeData theme = Theme.of(context);
+
     return Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text("Select Feedback Type"),
-         
-            // SizedBox(height: 10),
-                Text("Quick"),
-            // SizedBox(height: 10),
-              Text("Behavourial"),
-              Text("Technical - Coming soon"),
-          
-          
-           NextAndBackPairButtons(moveToNextOrPrevFormOnClickCopy: moveToNextOrPrevFormOnClickCopy),
+ 
+      // ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          // ToggleButtons with a single selection.
+          Text('Select Feedback Type', style:  Theme.of(context).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.bold)),
+          const SizedBox(height: 5),
+          ToggleButtons(
+              direction: Axis.vertical,
+              renderBorder: false,
+              onPressed: (int index) {
+                setState(() {
+                  // The button that is tapped is set to true, and the others to false.
+                  for (int i = 0; i < selectedFeedback.length; i++) {
+                    selectedFeedback[i] = i == index;
+                  }
+                });
+              },
+              // borderRadius: const BorderRadius.all(Radius.circular(8)),
+              // selectedBorderColor: Colors.blue,
+              // selectedColor: Colors.white,
+              fillColor: Colors.white,
+              color: Colors.white,
+              constraints: const BoxConstraints(
+                minHeight: 40.0,
+                minWidth: 40.0,
+              ),
+              isSelected: selectedFeedback,
+              children: List<Widget>.generate(
+                  3,
+                  (index) => Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            
+                            border: Border.all(width:2, color: selectedFeedback[index]? Color.fromARGB(255, 0, 132, 248):Colors.black38),
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                          ),
+                          padding: const EdgeInsets.all(20),
+                          // width: 160,
+                          // height: 70,
+                          alignment: Alignment.topLeft,
+                          child: 
+                            _interviewFeedbackTypeTextWidgets[index],
+                          
+                        ),
+                      ))),
+        
+        
+        Text("TODO: Include Picture of Behavourial and Technical Photo (show code base style)"),
 
-          ],
+         NextAndBackPairButtons(moveToNextOrPrevFormOnClickCopy: moveToNextOrPrevFormOnClickCopy),
 
-        ),
-      );
+        ],
+      
+      
+      ),
+    );
   }
 }
-
-Future<http.Response> fetchAlbum() {
-  return http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
-}
-
-
-final _interviewFeedbackTypes = [
-  "Simple",
-  "Behavourial",
-  "Technical"
-];
-
-
-
-
-// class typeButton extends
-
-
-// (int index){
-//   return OutlinedButton
-// }
 
 //TODO:  Refactor these for all the other files
 class NextAndBackPairButtons extends StatelessWidget {
@@ -156,11 +168,11 @@ class NextAndBackPairButtons extends StatelessWidget {
       // crossAxisAlignment: CrossAxisAlignment.end,
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
-        BackButton(moveToNextOrPrevFormOnClickCopy: moveToNextOrPrevFormOnClickCopy),
-        SizedBox(width:3),
+        BackButton(
+            moveToNextOrPrevFormOnClickCopy: moveToNextOrPrevFormOnClickCopy),
+        SizedBox(width: 3),
         NextButton(
-            moveToNextOrPrevFormOnClickCopy:
-                moveToNextOrPrevFormOnClickCopy)
+            moveToNextOrPrevFormOnClickCopy: moveToNextOrPrevFormOnClickCopy)
       ],
     );
   }
@@ -184,7 +196,6 @@ class BackButton extends StatelessWidget {
   }
 }
 
-
 class NextButton extends StatelessWidget {
   const NextButton({
     super.key,
@@ -196,24 +207,22 @@ class NextButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          foregroundColor: Colors.white, //change background color of button
-          backgroundColor: Color.fromARGB(206, 58, 82, 204), //
-          elevation: 10.0,
-        ),
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.white, //change background color of button
+        backgroundColor: Color.fromARGB(206, 58, 82, 204), //
+        elevation: 10.0,
+      ),
 
-        //           ButtonStyle(
-        // backgroundColor: WidgetStateProperty.all<Color>(),
-        // textStyle:   WidgetStateProperty.all<TextStyle>(TextStyle(color:Colors.white)),
-        //           ),
-        onPressed: () {
-
-            moveToNextOrPrevFormOnClickCopy(true);
-          } 
-        ,
-        child: Text(
-            'Submit',
-          ),
-        );
+      //           ButtonStyle(
+      // backgroundColor: WidgetStateProperty.all<Color>(),
+      // textStyle:   WidgetStateProperty.all<TextStyle>(TextStyle(color:Colors.white)),
+      //           ),
+      onPressed: () {
+        moveToNextOrPrevFormOnClickCopy(true);
+      },
+      child: Text(
+        'Submit',
+      ),
+    );
   }
 }
